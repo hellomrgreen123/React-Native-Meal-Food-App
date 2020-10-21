9779bbd94cacc8f1839a6f943edd99f1ba2182ce
hellomrgreen123 <oisinomaolcahllan@gmail.com>
2020-10-21 10:41:04 +0200

    Create CategoriesScreen.js

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { FlatList } from "react-native-gesture-handler";
+import { CATEGORIES } from "../data/dummy-data";
+import CategoryGridTile from "../components/CategoryGridTile";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import HeaderButton from "../components/HeaderButton";
+const CatergoriesScreen = (props) => {
+  const renderGridItem = (itemData) => {
+    return (
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onSelect={() => {
+          props.navigation.navigate({
+            routeName: "CategoryMeals",
+            params: {
+              catergoryId: itemData.item.id,
+            },
+          });
+        }}
+      />
+    );
+  };
+  return (
+    <FlatList
+      keyExtractor={(item, index) => item.id}
+      data={CATEGORIES}
+      renderItem={renderGridItem}
+      numColumns={2}
+      style={{ width: "100%" }}
+    />
+  );
+};
+
+CatergoriesScreen.navigationOptions = (navData) => {
+  return {
+    headerTitle: "Meal Catergories",
+    headerLeft: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Menu"
+          iconName="ios-menu"
+          onPress={() => {
+            navData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+    ),
+  };
+};
+
+export default CatergoriesScreen;
